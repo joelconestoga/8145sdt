a3bb710c6abb510553b1f064a96df3c40cba7f8a
joel matsu <matsu.joel@gmail.com>
2017-03-21 16:10:37 -0400

    don't remember what I've changed here, so far

---
+++ BaseballTickets/BasebalTickets/BaseballTicketsTest/ACalculatorTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BaseballTicketsTest
+{
+    [TestClass]
+    public class ACalculatorTest
+    {
+        ACalculator
+
+        [TestMethod]
+        public void getTotal()
+        {
+        }
+    }
+}
